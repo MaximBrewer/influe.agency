d435ebbcdb32da8cd3a962a95a91c84cbf68278d
MaximBrewer <pimax1978@icloud.com>
2023-09-24 17:10:54 +0600

    sdf

---
+++ database/migrations/2023_09_24_170125_create_partners_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order')->default(1000);
+            $table->string('image', 1023)->nullable();
+            $table->string('title', 1023);
+            $table->string('url', 1023)->nullable();
+            $table->longText('text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('partners');
+    }
+};
