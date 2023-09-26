85bf0b7dad7ad4c062db8b9c33221b92b00eb517
MaximBrewer <pimax1978@icloud.com>
2023-09-26 10:47:50 +0600

    sff

---
+++ database/migrations/2023_09_26_095307_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedTinyInteger('type')->default(0);
+            $table->string('avatar')->nullable();
+            $table->string('post')->nullable();
+            $table->string('degree')->nullable();
+            $table->string('experience')->nullable();
+            $table->longtext('text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
