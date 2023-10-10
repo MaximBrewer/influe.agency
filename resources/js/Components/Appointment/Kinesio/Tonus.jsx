d4f2e6871c0e57b3cb84cf982206a6f4c41b1c50
MaximBrewer <pimax1978@icloud.com>
2023-10-10 23:23:23 +0600

    sdf

--- resources/js/Components/Appointment/Kinesio/Tonus.jsx
+++ resources/js/Components/Appointment/Kinesio/Tonus.jsx
@@ -39,14 +39,7 @@ export default (props) => {
         <div className="flex gap-16">
             <div className="grow">
                 <div className="flex items-center gap-1 mb-2">
-                    <span>Карта тела (оценка: Го; Гр; ( </span>
-                    <input
-                        type="checkbox"
-                        disabled
-                        className={'border-gray-300 text-purple-900 shadow-sm focus:ring-purple-900 rounded-sm'}
-                        defaultChecked={true}
-                    />
-                    <span> если есть спастичность))</span>
+                    <span>Карта тела (оценка: Го; Гр; + если есть спастичность; 0&nbsp;-&nbsp;норма)</span>
                 </div>
                 <div className="flex items-center gap-1 mb-2">
                     <span>ВП – верхний перекрест (</span>
@@ -71,52 +64,19 @@ export default (props) => {
 
                 <div className="font-medium mt-4 mb-2">Сумма </div>
                 <label className="flex items-center gap-2 mb-2">
-                    <span className="w-8">Го</span>
-                    <input
-                        type="checkbox"
-                        id={`tonus-go`}
-                        onChange={e => setData(prev => {
-                            const data = { ...prev }
-                            const kinesio = data.kinesio
-                            if (!kinesio.tonus) kinesio.tonus = {}
-                            kinesio.tonus.go = e.target.checked
-                            return data
-                        })}
-                        defaultChecked={data.kinesio.tonus && data.kinesio.tonus.go}
-                        className={'leading-none block border-gray-300 text-purple-900 shadow-sm focus:ring-purple-900 rounded-sm'}
-                    />
+                    <span className="w-8">Го</span> 5
                 </label>
                 <label className="flex items-center gap-2 mb-2">
-                    <span className="w-8">Гр</span>
-                    <input
-                        type="checkbox"
-                        id={`tonus-gr`}
-                        onChange={e => setData(prev => {
-                            const data = { ...prev }
-                            const kinesio = data.kinesio
-                            if (!kinesio.tonus) kinesio.tonus = {}
-                            kinesio.tonus.gr = e.target.checked
-                            return data
-                        })}
-                        defaultChecked={data.kinesio.tonus && data.kinesio.tonus.gr}
-                        className={'leading-none block border-gray-300 text-purple-900 shadow-sm focus:ring-purple-900 rounded-sm'}
-                    />
+                    <span className="w-8">Гр</span> 6
                 </label>
                 <label className="flex items-center gap-2 mb-2">
-                    <span className="w-8">Тип</span>
-                    <input
-                        type="checkbox"
-                        id={`tonus-tip`}
-                        onChange={e => setData(prev => {
-                            const data = { ...prev }
-                            const kinesio = data.kinesio
-                            if (!kinesio.tonus) kinesio.tonus = {}
-                            kinesio.tonus.tip = e.target.checked
-                            return data
-                        })}
-                        defaultChecked={data.kinesio.tonus && data.kinesio.tonus.tip}
-                        className={'leading-none block border-gray-300 text-purple-900 shadow-sm focus:ring-purple-900 rounded-sm'}
-                    />
+                    <span className="w-8">+</span> 6
+                </label>
+                <label className="flex items-center gap-2 mb-2">
+                    <span className="w-8">0</span> 6
+                </label>
+                <label className="flex items-center gap-2 mb-2">
+                    <span className="w-8">Тип</span> Гр
                 </label>
             </div>
             <div className="shrink-0 p-24">
@@ -172,53 +132,19 @@ export default (props) => {
                     </label>
 
 
-                    <div className="absolute left-1/2 top-8 -translate-x-1/2 -ml-0.5">
-                        <div className="grid grid-cols-2 gap-2">
-                            <div className="flex gap-1 items-center">
-                                <span>р</span>
-                                <input
-                                    type="kinesio"
-                                    onChange={e => setData(prev => {
-                                        const data = { ...prev }
-                                        const kinesio = data.kinesio
-                                        if (!kinesio.tonus) kinesio.tonus = {}
-                                        kinesio.tonus.headp = e.target.value
-                                        return data
-                                    })}
-                                    value={data.kinesio.tonus ? data.kinesio.tonus.headp : ``}
-                                    className={'p-0 w-8 border-0 box-shadow-none border-b leading-none outline-none text-center'}
-                                />
-                            </div>
-                            <div className="flex gap-1 items-center">
-                                <span>с</span>
-                                <input
-                                    type="kinesio"
-                                    onChange={e => setData(prev => {
-                                        const data = { ...prev }
-                                        const kinesio = data.kinesio
-                                        if (!kinesio.tonus) kinesio.tonus = {}
-                                        kinesio.tonus.headc = e.target.value
-                                        return data
-                                    })}
-                                    value={data.kinesio.tonus ? data.kinesio.tonus.headc : ``}
-                                    className={'p-0 w-8 border-0 box-shadow-none border-b leading-none outline-none text-center'}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `headp` },
                         { label: `c`, name: `headc` }
-                    ]} className={`absolute left-1/2 top-7 -translate-x-1/2`} />
+                    ]} className={`absolute left-1/2 top-7 -translate-x-1/2`} /> */}
 
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `neckp` },
                         { label: `c`, name: `neckc` }
                     ]} className={`absolute left-1/2 top-16 mt-1 -translate-x-1/2`} />
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `llshp` }
-                    ]} className={`absolute left-1/2 top-[8.375rem] -translate-x-1/2 -ml-[10.5rem]`} />
+                    ]} className={`absolute left-1/2 top-[8.375rem] -translate-x-1/2 -ml-[10.5rem]`} /> */}
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `lshp` },
                         { label: `c`, name: `lshc` }
@@ -227,24 +153,24 @@ export default (props) => {
                         { label: `р`, name: `rshp` },
                         { label: `c`, name: `rshc` }
                     ]} className={`absolute left-1/2 top-[7.5rem] -translate-x-1/2 ml-24`} />
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `rrshp` }
-                    ]} className={`absolute left-1/2 top-[8.375rem] -translate-x-1/2 ml-[10.5rem]`} />
+                    ]} className={`absolute left-1/2 top-[8.375rem] -translate-x-1/2 ml-[10.5rem]`} /> */}
 
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `backp` },
                         { label: `c`, name: `backc` }
                     ]} className={`absolute left-1/2 top-40 -translate-x-1/2`} />
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `bbackp` },
                         { label: `c`, name: `bbackc` }
-                    ]} className={`absolute left-1/2 top-[14rem] -translate-x-1/2`} />
+                    ]} className={`absolute left-1/2 top-[14rem] -translate-x-1/2`} /> */}
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `llelbp` },
                         { label: `c`, name: `llelbc` }
-                    ]} className={`absolute left-1/2 top-[16.375rem] -translate-x-1/2 -ml-[12.5rem]`} />
+                    ]} className={`absolute left-1/2 top-[16.375rem] -translate-x-1/2 -ml-[12.5rem]`} /> */}
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `lelbp` },
                         { label: `c`, name: `lelbc` }
@@ -253,25 +179,25 @@ export default (props) => {
                         { label: `р`, name: `relbp` },
                         { label: `c`, name: `relbc` }
                     ]} className={`absolute left-1/2 top-[15.5rem] -translate-x-1/2 ml-[7rem]`} />
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `rrelbp` },
                         { label: `c`, name: `rrelbc` }
-                    ]} className={`absolute left-1/2 top-[16.375rem] -translate-x-1/2 ml-[12.5rem]`} />
+                    ]} className={`absolute left-1/2 top-[16.375rem] -translate-x-1/2 ml-[12.5rem]`} /> */}
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `sbackp` },
                         { label: `c`, name: `sbackc` }
-                    ]} className={`absolute left-1/2 top-[18.25rem] -translate-x-1/2`} />
+                    ]} className={`absolute left-1/2 top-[18.25rem] -translate-x-1/2`} /> */}
 
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `ccxp` },
                         { label: `c`, name: `ccxc` }
                     ]} className={`absolute left-1/2 top-[20.75rem] -translate-x-1/2`} />
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `llpalmp` },
                         { label: `c`, name: `llpalmc` }
-                    ]} className={`absolute left-1/2 top-[22.5rem] -translate-x-1/2 -ml-[15rem]`} />
+                    ]} className={`absolute left-1/2 top-[22.5rem] -translate-x-1/2 -ml-[15rem]`} /> */}
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `lpalmp` },
                         { label: `c`, name: `lpalmc` }
@@ -280,10 +206,10 @@ export default (props) => {
                         { label: `р`, name: `rpalmp` },
                         { label: `c`, name: `rpalmc` }
                     ]} className={`absolute left-1/2 top-[21.675rem] -translate-x-1/2 ml-[9.5rem]`} />
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `rrpalmp` },
                         { label: `c`, name: `rrpalmc` }
-                    ]} className={`absolute left-1/2 top-[22.5rem] -translate-x-1/2 ml-[15rem]`} />
+                    ]} className={`absolute left-1/2 top-[22.5rem] -translate-x-1/2 ml-[15rem]`} /> */}
 
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `lbuttp` },
@@ -299,15 +225,15 @@ export default (props) => {
                     ]} className={`absolute right-[3.75rem] top-[24.875rem]`} />
 
                     <Field data={data} setData={setData} rounded={true} fields={[
-                        { label: `р`, name: `lfgrsp` },
+                        // { label: `р`, name: `lfgrsp` },
                         { label: `c`, name: `lfgrsc` }
                     ]} className={`absolute left-1.5 top-[26.375rem]`} />
                     <Field data={data} setData={setData} rounded={true} fields={[
-                        { label: `р`, name: `rfgrsp` },
+                        // { label: `р`, name: `rfgrsp` },
                         { label: `c`, name: `rfgrsc` }
                     ]} className={`absolute right-1.5 top-[26.375rem]`} />
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `lhipsp` },
                         { label: `c`, name: `lhipsc` },
                         { label: `о`, name: `lhipso` },
@@ -318,13 +244,13 @@ export default (props) => {
                         { label: `c`, name: `rhipsc` },
                         { label: `нр`, name: `rhipso` },
                         { label: `вр`, name: `rhipspr` }
-                    ]} className={`absolute right-[5.25rem] top-[28.875rem]`} />
+                    ]} className={`absolute right-[5.25rem] top-[28.875rem]`} /> */}
 
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `llkneep` },
                         { label: `c`, name: `llkneec` }
-                    ]} className={`absolute left-1/2 top-[34.5rem] -translate-x-1/2 -ml-[8rem]`} />
+                    ]} className={`absolute left-1/2 top-[34.5rem] -translate-x-1/2 -ml-[8rem]`} /> */}
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `lkneep` },
                         { label: `c`, name: `lkneec` }
@@ -333,16 +259,16 @@ export default (props) => {
                         { label: `р`, name: `rkneep` },
                         { label: `c`, name: `rkneec` }
                     ]} className={`absolute left-1/2 top-[33.675rem] -translate-x-1/2 ml-[3rem]`} />
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `rrkneep` },
                         { label: `c`, name: `rrkneec` }
-                    ]} className={`absolute left-1/2 top-[34.5rem] -translate-x-1/2 ml-[8rem]`} />
+                    ]} className={`absolute left-1/2 top-[34.5rem] -translate-x-1/2 ml-[8rem]`} /> */}
 
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `llanklep` },
                         { label: `c`, name: `llanklec` }
-                    ]} className={`absolute left-1/2 top-[44.5rem] -translate-x-1/2 -ml-[8rem]`} />
+                    ]} className={`absolute left-1/2 top-[44.5rem] -translate-x-1/2 -ml-[8rem]`} /> */}
                     <Field data={data} setData={setData} rounded={true} fields={[
                         { label: `р`, name: `lanklep` },
                         { label: `c`, name: `lanklec` }
@@ -351,27 +277,27 @@ export default (props) => {
                         { label: `р`, name: `ranklep` },
                         { label: `c`, name: `ranklec` }
                     ]} className={`absolute left-1/2 top-[43.675rem] -translate-x-1/2 ml-[3rem]`} />
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `rranklep` },
                         { label: `c`, name: `rranklec` }
-                    ]} className={`absolute left-1/2 top-[44.5rem] -translate-x-1/2 ml-[8rem]`} />
+                    ]} className={`absolute left-1/2 top-[44.5rem] -translate-x-1/2 ml-[8rem]`} /> */}
 
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `llfeetp` },
                         { label: `c`, name: `llfeetc` }
-                    ]} className={`absolute left-1/2 top-[48.5rem] -translate-x-1/2 -ml-[9rem]`} />
+                    ]} className={`absolute left-1/2 top-[48.5rem] -translate-x-1/2 -ml-[9rem]`} /> */}
                     <Field data={data} setData={setData} rounded={true} fields={[
-                        { label: `р`, name: `lfeetp` },
+                        // { label: `р`, name: `lfeetp` },
                         { label: `c`, name: `lfeetc` }
                     ]} className={`absolute left-1/2 top-[47.675rem] -translate-x-1/2 -ml-[4rem]`} />
                     <Field data={data} setData={setData} rounded={true} fields={[
-                        { label: `р`, name: `rfeetp` },
+                        // { label: `р`, name: `rfeetp` },
                         { label: `c`, name: `rfeetc` }
                     ]} className={`absolute left-1/2 top-[47.675rem] -translate-x-1/2 ml-[4rem]`} />
-                    <Field data={data} setData={setData} rounded={false} fields={[
+                    {/* <Field data={data} setData={setData} rounded={false} fields={[
                         { label: `р`, name: `rrfeetp` },
                         { label: `c`, name: `rrfeetc` }
-                    ]} className={`absolute left-1/2 top-[48.5rem] -translate-x-1/2 ml-[9rem]`} />
+                    ]} className={`absolute left-1/2 top-[48.5rem] -translate-x-1/2 ml-[9rem]`} /> */}
 
                 </div>
             </div>
